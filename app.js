000baadddad0544105b92ecddfde7ca64177697b
AnHuy <92672519+danganhuy@users.noreply.github.com>
2024-10-08 08:51:32 +0700

    m

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ function ToConsole() {
 function  ToAlert() {
     alert("Hello")
 }
-function  ToWeb() {
+function  ToElement() {
     document.getElementById("message").innerHTML = "Hello"
+}
+function  ToWeb() {
+    document.write("Hello")
 }
 document.getElementById("console").addEventListener("click", ToConsole);
 document.getElementById("alert").addEventListener("click", ToAlert);
+document.getElementById("element").addEventListener("click", ToElement);
 document.getElementById("web").addEventListener("click", ToWeb);
